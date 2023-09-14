1771c698c00fcecc26d042d9554ad657297188b8
MiguelGomezG <miguelgomezgonzalez07@gmail.com>
2023-09-14 10:43:54 +0200

    14/09/2023

---
+++ 2DAW/DEC/UD1/Ejer1-UD1. Integración HTML5+JSTarea/Integracion_JS+HTML_separado/integracion.js
@@ -0,0 +1,7 @@
+function suma() {
+  numero1 = parseInt(prompt("Introduce el primer valor a sumar: "));
+  numero2 = parseInt(prompt("Introduce el segundo valor a sumar: "));
+  let resultado = numero1 + numero2;
+  alert("El resultado es: " + resultado);
+  return resultado;
+}
