f635583691b8a2b04040a77c92495e258aeecabd
MiguelGomezG <miguelgomezgonzalez07@gmail.com>
2023-09-20 14:13:11 +0200

    20/09/2023

--- 2DAW/DEC/UD 1. Javascript/Ejer11-UD1. Saludos. DATE/saludoss.js
+++ 2DAW/DEC/UD 1. Javascript/Ejer11-UD1. Saludos. DATE/saludoss.js
@@ -3,13 +3,13 @@ const saludamos = (nombre) => {
   var horaDividida = horaCompleta.split(":");
   var hora = parseInt(horaDividida[0], 10);
   if ([7, 8, 9, 10, 11, 12, 13, 14].includes(hora)) {
-    return "Buenos Dias " + nombre;
+    return "Buenos Dias " + nombre +" son las "+horaCompleta;
   }
   if ([15, 16, 17, 18, 19, 20, 21].includes(hora)) {
-    return "Buenos Tardes " + nombre;
+    return "Buenos Tardes " + nombre +" son las "+horaCompleta;
   }
   if ([22, 23, 24, 12, 1, 2, 3, 4, 5, 6].includes(hora)) {
-    return "Buenos Noches " + nombre;
+    return "Buenos Noches " + nombre +" son las "+horaCompleta;
   }
 };
 console.log(saludamos("Miguel"));
