a8fd8cd33c57cce05a8e959e7487d3f9e6309c71
MiguelGomezG <miguelgomezgonzalez07@gmail.com>
2023-09-26 09:56:58 +0200

    26/09/2023

--- 2DAW/DEC/UD 1. Javascript/IMPUTUABLES/Ejer14-UD1. Clases #2. HerenciaTarea/clases3.js
+++ 2DAW/DEC/UD 1. Javascript/IMPUTUABLES/Ejer14-UD1. Clases #2. HerenciaTarea/clases3.js
@@ -1,52 +1,85 @@
 // PADRE
 class Usuario {
-    constructor(){
-        
-    }
+  constructor(nombre, email, password) {
+    this.nombre = nombre;
+    this.email = email;
+    this.password = password;
+  }
 
-    saludo(){
-    }
+  saludo() {
+    console.log("Hola, soy " + this.nombre);
+  }
 
-    login (email,password){
-        
-    }
-    
+  login(email, password) {}
 }
 
-// HIJO alumnos de DAW
-class AlumnoASIR { 
-    constructor(nombre, email, password){
-        this.cursos = []; // aÃ±adimos cursos
-        this.activo = false;
-    }    
-    
-    activar (){
-    }
+class AlumnoDaw extends Usuario {
+  constructor(nombre, email, password) {
+    super(nombre, email, password);
+    this.cursos = [];
+    this.activo = false;
+  }
 
-    login(email,password){ // sobreescribimos el login
-        
-    }
-    
-    AgregarCurso(curso) {
-        this.cursos.push(curso);
-    }   
-}    
+  activar() {
+    this.activo = true;
+  }
 
-class AlumnoDaw { 
-        constructor(nombre, email, password){
-        this.cursos = []; // aÃ±adimos cursos
-        this.activo = false;
-     
+  login(email, password) {
+    if (email === this.email && password === this.password) {
+      console.log(
+        "True at usuarioDAW.login(`" + email + "`, `" + password + "`)"
+      );
+    } else {
+      console.log(
+        "False at usuarioDAW.login(`" + email + "`, `" + password + "`)"
+      );
     }
-    activar (){
-        
-    }
+  }
 
-    login(email,password){ 
+  AgregarCurso(curso) {
+    this.cursos.push(curso);
+  }
+}
+
+class AlumnoASIR extends AlumnoDaw {
+  constructor(nombre, email, password) {
+    super(nombre, email, password);
+    this.cursos = [];
+    this.activo = false;
+  }
+
+  login(email, password) {
+    if (email === this.email && password === this.password) {
+      console.log(
+        "True at usuarioASIR.login(`" + email + "`, `" + password + "`)"
+      );
+    } else {
+      console.log(
+        "False at usuarioASIR.login(`" + email + "`, `" + password + "`)"
+      );
     }
-    
-    AgregarCurso(curso) {
-        this.cursos.push(curso);
-    }  
-} 
-// INSTANCIAMOS 
+  }
+}
+
+// Probamos
+const alumnodaww = new AlumnoDaw(
+  "Miguel",
+  "miguelgomez@gmail.com",
+  "123456789"
+);
+alumnodaww.saludo();
+alumnodaww.activar();
+alumnodaww.AgregarCurso("SMX");
+alumnodaww.login("miguelgomez@gmail.com", "123456789");
+alumnodaww.login("junitoplantounclavito@gmail.com", "123456789");
+
+const alumnoasirr = new AlumnoASIR(
+  "Juan",
+  "junitoplantounclavito@gmail.com",
+  "123456789"
+);
+alumnoasirr.saludo();
+alumnoasirr.activar();
+alumnoasirr.AgregarCurso("ASIR");
+alumnoasirr.login("junitoplantounclavito@gmail.com", "123456789");
+alumnoasirr.login("miguelgomez@gmail.com", "123456789");
---
+++ 2DAW/DEC/UD 1. Javascript/IMPUTUABLES/Ejer15-UD1. Clases #3. Get y Set/clases3.js
@@ -0,0 +1,61 @@
+class AlumnoDaw {
+  constructor(nombre, apellido, email, edad) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+    this._email = email;
+    this._edad = edad;
+  }
+
+  get nombre() {
+    return this._nombre;
+  }
+
+  set nombre(nuevoNombre) {
+    this._nombre = nuevoNombre;
+  }
+
+  get apellido() {
+    return this._apellido;
+  }
+
+  set apellido(nuevoApellido) {
+    this._apellido = nuevoApellido;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  set email(nuevoEmail) {
+    this._email = nuevoEmail;
+  }
+
+  get edad() {
+    return this._edad;
+  }
+
+  set edad(nuevaEdad) {
+    if (nuevaEdad >= 0) {
+      this._edad = nuevaEdad;
+    } else {
+      console.log("La edad no puede ser un valor negativo.");
+    }
+  }
+}
+
+const alumno = new AlumnoDaw("Juan", "Pérez", "juan@example.com", 25);
+
+console.log("Nombre:", alumno.nombre);
+console.log("Apellido:", alumno.apellido);
+console.log("Email:", alumno.email);
+console.log("Edad:", alumno.edad);
+
+alumno.nombre = "Luis";
+alumno.apellido = "Gómez";
+alumno.email = "luis@example.com";
+alumno.edad = 30;
+
+console.log("Nombre modificado:", alumno.nombre);
+console.log("Apellido modificado:", alumno.apellido);
+console.log("Email modificado:", alumno.email);
+console.log("Edad modificado:", alumno.edad);
