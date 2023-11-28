66f6d86735dae7d78b452913ff25441a8e2bb29c
MiguelGomezG <miguelgomezgonzalez07@gmail.com>
2023-11-28 12:24:40 +0100

    28/11/2023

--- 2DAW/DEC/UD 2. AJAX/PUNTUABLES/Ejer4-UD2. Puntuable. Creación de una API propia/js/login.js
+++ 2DAW/DEC/UD 2. AJAX/PUNTUABLES/Ejer4-UD2. Puntuable. Creación de una API propia/js/login.js
@@ -1,26 +1,33 @@
-document.getElementById("loginForm").addEventListener("submit", function (event) {
-  event.preventDefault();
+var userData = {
+  name: document.getElementById("nombre").value,
+  email: document.getElementById("email").value,
+  password: document.getElementById("password").value
+};
 
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+// Crea una nueva instancia de XMLHttpRequest
+var xhr = new XMLHttpRequest();
 
-  // Realiza una solicitud HTTP para autenticar al usuario utilizando los datos ingresados
-  fetch('http://localhost:8000/api/login', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email, password })
-})
-.then(response => response.json())
-.then(data => {
-    console.log("Respuesta del servidor:", data);
-    alert("Bienvenido");
-})
-.catch(error => {
-    console.error("Error al realizar la solicitud:", error);
-    alert("Adiós intruso");
-});
+// Configura la solicitud
+xhr.open("POST", "http://localhost:8000/api/user/", true);
+xhr.setRequestHeader("Content-Type", "application/json");
 
+// Maneja el evento de carga de la solicitud
+xhr.onload = function () {
+  if (xhr.status === 200) {
+      // Éxito, maneja la respuesta según sea necesario
+      var data = JSON.parse(xhr.responseText);
+      console.log("Respuesta del servidor:", data);
+  } else {
+      // Error en la solicitud
+      console.error("Error al realizar la solicitud:", xhr.statusText);
+  }
+};
 
-});
+// Maneja el evento de error de la solicitud
+xhr.onerror = function () {
+  console.error("Error de red al intentar realizar la solicitud.");
+};
+
+// Convierte el objeto userData a una cadena JSON y envíalo en el cuerpo de la solicitud
+var jsonData = JSON.stringify(userData);
+xhr.send(jsonData);
